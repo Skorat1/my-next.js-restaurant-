f73ed302cdbcd6ed901fcfa59c2dc8766939c994
Aapka GitHub Username <satishkorat890@gmail.com>
2026-08-14 10:34:59 +0530

    change  a admin mail id

--- middleware/admin.js
+++ middleware/admin.js
@@ -3,10 +3,12 @@ const User = require('../models/User');
 module.exports = async function (req, res, next) {
   try {
     const user = await User.findById(req.user._id);
+    // admin login error //
     if (user.role !== 'admin') {
       return res.status(403).json({ msg: 'Access denied. Not an admin.' });
     }
     next();
+    // server error admin panel not any changes//
   } catch (err) {
     res.status(500).send('Server error');
   }
