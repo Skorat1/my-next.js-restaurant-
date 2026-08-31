5d09921528398734809580c0488909def5bdcf92
Aapka GitHub Username <satishkorat890@gmail.com>
2026-08-31 16:40:05 +0530

    fix(lambda): export standard req/res handler wrapper in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
 const app = require('../server');
 
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    return app(req, res);
+  } catch (err) {
+    console.error('Unhandled Lambda Error in api/index.js:', err);
+    return res.status(500).json({
+      error: 'Lambda Execution Error',
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+};
